70851e583dd7cfe629021b501428e1e2746ce468
MRahulRK <rahulharidasrkm@gmail.com>
2025-08-10 23:01:10 +0530

    Baseline path Changed

--- src/test/java/Base/BaseTestLogin.java
+++ src/test/java/Base/BaseTestLogin.java
@@ -1,7 +1,9 @@
 package Base;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
@@ -24,12 +26,39 @@ public class BaseTestLogin {
 	public void login() throws IOException {
 
 		if (driver == null) {
+			
+			
+//			fisPr = new FileInputStream(
+//				    System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator +
+//				    "resources" + File.separator + "TestData" + File.separator + "Config.Properties"
+//				);
+//			fisLoc = new FileInputStream(
+//				    System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator +
+//				    "resources" + File.separator + "TestData" + File.separator + "Locaters.Properties"
+//				);
+
+			
 			fisPr = new FileInputStream(
-					System.getProperty("user.dir") + "\\src\\test\\resources\\TestData\\Config.Properties");
+				    Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "TestData", "Config.Properties").toFile()
+				);
 			fisLoc = new FileInputStream(
-					System.getProperty("user.dir") + "\\src\\test\\resources\\TestData\\Locaters.Properties");
+				    Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "TestData", "Locaters.Properties").toFile()
+				);
+
+			
+	//		fisPr = new FileInputStream(
+	//				System.getProperty("user.dir") + "\\src\\test\\resources\\TestData\\Config.Properties");
+	//		fisLoc = new FileInputStream(
+	//				System.getProperty("user.dir") + "\\src\\test\\resources\\TestData\\Locaters.Properties");
 			pr.load(fisPr);
 			loc.load(fisLoc);
+			
+			if (fisLoc == null) {
+                throw new IOException("Config.Properties file not found in resources/TestData");
+            }
+			if (fisPr == null) {
+				throw new IOException("Locaters.Properties file not found in resources/TestData");
+			}
 
 		}
 
